da4f0e1746b54e0bb3d6935be97a71c35c2c4ed6
wkropat <wkropat@gmail.com>
2021-10-26 08:22:13 -0700

    worked on backend

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,39 @@
-// Get various html elements
-var weatherEl = document.getElementById("#weatherCard");
-var forecastEl = document.getElementById("#forecastCard");
-var searchLogEl = document.getElementById("#searchLog");
-var citySearch = document.getElementById("#citySearch");
+function init() {
+    // Get various html elements
+    // WeatherEl shows current days weather
+    const weatherEl = document.getElementById("#weatherCard");
+    // ForecastEl shows 5 day forcast
+    const forecastEl = document.getElementById("#forecastCard");
+    // SearchLogEl has city search form
+    const searchLogEl = document.getElementById("#searchLog");
+    // citySearch is the button to search cities
+    const searchBtn = document.getElementById("#searchBtn");
+    // ClearBtn Clears search history
+    const clearBtn = document.getElementById("#clearBtn");
+    // SearchLog is list of city searches (10 max)
+    const searchLog = document.getElementById("#searchLog");
+    // Save past searches to an array
+    var citiesSearched = [];
 
-// Save past searches to an array
-var citiesSearched = [];
+    function citySearch(event) {
+        event.preventDefault();
+        var city = citySearch.value;
+        getCurrentWeather(city);
+        getForecastWeather(city);
+        citiesSearched.unshift(city);
+        citySearch.value = "";
+        saveData();
+        addSearchedCity(city);
+        
+    }
+    function addCityEl() {
 
-function citySearch(event) {
-    event.preventDefault();
-    var city = citySearch.value;
-    getCurrentWeather(city);
-    getForecastWeather(city);
-    citiesSearched.unshift(city);
-    citySearch.value = "";
-    saveData();
-    addSearchedCity(city);
-    
+
+        
+    }
+    function saveData() {
+        localStorage.setItem("citiesSearched", citiesSearched);
+    }
 }
-function addCityEl() {
 
-
-    
-}
-function saveData() {
-    localStorage.setItem("citiesSearched", citiesSearched);
-}
+init();
